f584795d7085f70bf7ccac22e0d396b2f217a403
LucasKienast <lucas.kienast@hotmail.de>
2022-11-09 21:04:32 +0000

    changed count

--- c5-ec2-instance.tf
+++ c5-ec2-instance.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "jenkins-ec2-vm" {
   ami                    = data.aws_ami.amzlinux.id
   instance_type          = var.instance_type
+  count                  = 1
   key_name               = "terraform-key"
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
