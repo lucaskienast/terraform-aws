5399e9acfd93643c8a3374dddaf9e9399a1cfb49
LucasKienast <lucas.kienast@hotmail.de>
2022-11-16 08:17:03 -0800

    changed bash script to install maven

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Install Ansible
 sudo yum update -y
 sudo amazon-linux-extras install ansible2 -y
@@ -9,18 +10,20 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 amazon-linux-extras install epel
 amazon-linux-extras install java-openjdk11
-sudo yum install jenkins
+sudo yum install jenkins -y
 sudo systemctl enable jenkins
+sudo systemctl start jenkins
+jenkins --version
 
 # Install Git
-sudo yum install git
+sudo yum install git -y
+git --version
 
 # Install Maven
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
 sudo tar -xvzf apache-maven-3.8.6-bin.tar.gz
 mv apache-maven-3.8.6 /opt/
-M2_HOME='/opt/apache-maven-3.8.6'
-PATH="$M2_HOME/bin:$PATH"
-export PATH
-source .bash_profile
-mvn -v
+sudo echo "export M2_HOME=/opt/apache-maven-3.8.6" >> /home/ec2-user/.bash_profile
+sudo echo "export PATH=/opt/apache-maven-3.8.6/bin:$PATH" >> /home/ec2-user/.bash_profile
+source /home/ec2-user/.bash_profile
+mvn --version
