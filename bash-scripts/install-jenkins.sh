a53444317f321430e87bd9d1f6c370c07257735b
LucasKienast <lucas.kienast@hotmail.de>
2023-01-15 14:42:47 +0000

    updated maven version to 3.8.7

--- bash-scripts/install-jenkins.sh
+++ bash-scripts/install-jenkins.sh
@@ -18,10 +18,11 @@ sudo yum install git -y
 git --version
 
 # Install Maven
-sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
-sudo tar -xvzf apache-maven-3.8.6-bin.tar.gz
-mv apache-maven-3.8.6 /opt/
-sudo echo "export M2_HOME=/opt/apache-maven-3.8.6" >> /home/ec2-user/.bash_profile
-sudo echo "export PATH=/opt/apache-maven-3.8.6/bin:$PATH" >> /home/ec2-user/.bash_profile
+apache-maven-3.8.7-bin.tar.gz
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
+sudo tar -xvzf apache-maven-3.8.7-bin.tar.gz
+mv apache-maven-3.8.7 /opt/
+sudo echo "export M2_HOME=/opt/apache-maven-3.8.7" >> /home/ec2-user/.bash_profile
+sudo echo "export PATH=/opt/apache-maven-3.8.7/bin:$PATH" >> /home/ec2-user/.bash_profile
 source /home/ec2-user/.bash_profile
 mvn --version
