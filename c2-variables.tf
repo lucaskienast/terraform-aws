543385b328b06b2d089a5503cea2a359bd01e485
LucasKienast <lucas.kienast@hotmail.de>
2023-01-20 17:11:49 +0000

    added dockerhub credentials

--- c2-variables.tf
+++ c2-variables.tf
@@ -8,13 +8,3 @@ variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type        = string
 }
-
-variable "dockerhub_username" {
-  description = "Username for DockerHub"
-  type        = string
-}
-
-variable "dockerhub_password" {
-  description = "Password for DockerHub"
-  type        = string
-}
